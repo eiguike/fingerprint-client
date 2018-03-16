1ff8550c1af20b0971590d1023dd8ea84588e998
Henrique Eihara <rick.eihara@gmail.com>
2018-03-16 01:02:29 -0300

    feat: using pthreads to start server and fingerprint device, not ready yet

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <curl/curl.h>
 
 #include "server.h"
 #include "device.h"
 #include "fingerprint.h"
 
 DEVICE* gDevice = NULL;
+SERVER* gServer = NULL;
 
 char* gUrl = "http://localhost:3000";
 char* gPassword = "testelabpesquisa";
@@ -23,34 +26,85 @@ SignalHandler(
   exit(0);
 }
 
-int main() {
-  //SERVER * Server = InitializeServer(8000);
-  //Server->Start(Server);
+void*
+WebServiceThread (
+    void* Value
+    )
+{
+  printf("WebServiceThread Begin\n");
 
-#ifdef WIRINGPILIB
-  // Initializing WiringPI
-  printf("Initializing WiringPi...");
-  wiringPiSetup();
-  pinMode(DOOR, OUTPUT);
-  digitalWrite(DOOR, LOW);
-  printf("OK!\n");
-#endif
+  PROCESS_TYPE Type = *(PROCESS_TYPE*) Value;
 
-  signal(SIGINT, SignalHandler);
-  gDevice = Device_Init(VERIFY_PROCESS);
+  gServer = InitializeServer(8000);
+  if (gServer != NULL) {
+    gServer->Start(gServer);
+  }
+
+  printf("WebServiceThread End\n");
+  return NULL;
+}
+
+void*
+DeviceFingerprintThread (
+    void* Value
+    )
+{
+  PROCESS_TYPE Type = *(PROCESS_TYPE*) Value;
+  printf("DeviceFingerprintThread Begin\n");
+  gDevice = Device_Init(Type);
 
   if (gDevice == NULL) {
     printf("Something wrong happened...\n");
     goto FINISH;
   }
 
-  while(1) {
-    gDevice->Verify(gDevice);
-    if (gDevice->Outdated == 1) {
-      gDevice->Fingerprint->Update(gDevice->Fingerprint);
-      //gDevice->Fingerprint->Load(gDevice->Fingerprint);
-    }
+  switch(Type) {
+    case ENROLL_PROCESS:
+      while(1) {
+        gDevice->Enroll(gDevice);
+      }
+      break;
+    case VERIFY_PROCESS:
+      while(1) {
+        gDevice->Verify(gDevice);
+        if (gDevice->Outdated == 1) {
+          gDevice->Fingerprint->Update(gDevice->Fingerprint);
+          gDevice->Fingerprint->Load(gDevice->Fingerprint);
+        }
+      }
+      break;
   }
+
+FINISH:
+  printf("DeviceFingerprintThread End\n");
+  return NULL;
+}
+
+int main() {
+  //signal(SIGINT, SignalHandler);
+  //PROCESS_TYPE Type = ENROLL_PROCESS;
+  PROCESS_TYPE Type = VERIFY_PROCESS;
+
+  printf ("Initializing libcurl... ");
+  if (curl_global_init(CURL_GLOBAL_ALL) < 0) {
+    printf("FAILED!\n");
+    goto FINISH;
+  }
+  printf ("OK!\n");
+
+  gDevice = Device_Init(Type);
+  gDevice->Dispose(gDevice);
+  //DeviceFingerprintThread((void*)&Type);
+  //pthread_t WebService;
+  //pthread_t DeviceFingerprint;
+
+  //pthread_create(&WebService, NULL, WebServiceThread, (void*)&Type);
+  //pthread_create(&DeviceFingerprint, NULL, DeviceFingerprintThread, (void*)&Type);
+
+  //pthread_join(WebService, NULL);
+  //pthread_join(DeviceFingerprint, NULL);
+  //
+  curl_global_cleanup();
 FINISH:
   return 0;
 }
